0fb78a7817e3da19f0884202d481f71dceb1c550
Julia Mus <zamowienia.eros@gmail.com>
2026-05-24 00:48:10 +0200

    fix van fbx loader

--- app/components/Van.tsx
+++ app/components/Van.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { FBXLoader } from "three-stdlib";
-import { useLoader } from "@react-three/fiber";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
 import { useRef } from "react";
 import type { Object3D } from "three";
 
@@ -11,19 +10,13 @@ function Model() {
   const fbx = useLoader(FBXLoader, "/models/van.FBX");
   const ref = useRef<Object3D | null>(null);
 
- useFrame(({ mouse }) => {
-  if (ref.current) {
-
-    ref.current.rotation.y += (
-      mouse.x * 0.8 - ref.current.rotation.y
-    ) * 0.03;
-
-    ref.current.rotation.x += (
-      -mouse.y * 0.2 - ref.current.rotation.x
-    ) * 0.03;
-
-  }
-});
+  useFrame(({ mouse }) => {
+    if (ref.current) {
+      ref.current.rotation.y += (mouse.x * 0.8 - ref.current.rotation.y) * 0.03;
+      ref.current.rotation.x +=
+        (-mouse.y * 0.2 - ref.current.rotation.x) * 0.03;
+    }
+  });
 
   return (
     <primitive
@@ -37,7 +30,7 @@ function Model() {
 
 export default function Van() {
   return (
-    <div className="w-full h-[500px]">
+    <div className="h-[500px] w-full">
       <Canvas camera={{ position: [0, 1, 5] }}>
         
         <ambientLight intensity={0.5} />
